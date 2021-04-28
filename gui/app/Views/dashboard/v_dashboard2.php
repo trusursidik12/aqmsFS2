a9d2f0816a0729970790e84258715d4a3e1d4c49
Irwan Antonio <irwanantonio2708@gmail.com>
2021-04-28 15:41:22 +0700

    STYLE(bg) important color

--- gui/app/Views/dashboard/v_dashboard2.php
+++ gui/app/Views/dashboard/v_dashboard2.php
@@ -148,11 +148,11 @@
                                     <div class="col-md-4 my-2 ">
                                         <div class="bg-success rounded px-3 py-2">
                                             <div class="d-flex justify-content-between align-items-center">
-                                                <h2 class="h6 text-white">Temperatur</h2>
-                                                <h2 class="h6 text-white">°</h2>
+                                                <h2 class="h6">Temperatur</h2>
+                                                <h2 class="h6">°</h2>
                                             </div>
                                             <div class="d-flex justify-content-end">
-                                                <h1 class="h1 text-white">23</h1>
+                                                <h1 class="h1">23</h1>
                                             </div>
                                         </div>
                                     </div>
