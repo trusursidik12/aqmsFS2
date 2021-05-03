3271b0c95c3432906a62b99c6f42779686aa9daf
warih suryono <warih_suryono@yahoo.com>
2021-05-03 13:00:42 +0700

    aaa

--- gui/app/Commands/FormulaMeasurementLogs.php
+++ gui/app/Commands/FormulaMeasurementLogs.php
@@ -90,8 +90,9 @@ class FormulaMeasurementLogs extends BaseCommand
 
 		foreach ($this->parameters->where("is_view", 1)->findAll() as $parameter) {
 			@eval("\$data[$parameter->id] = $parameter->formula;");
-			$sensor_value = @$this->sensor_values->where("id", $parameter->sensor_value_id)->findAll()[0];
-			$sensor_value = @$sensor[@$sensor_value->sensor_reader_id * 1][@$sensor_value->pin * 1] * 1;
+			// $sensor_value = @$this->sensor_values->where("id", $parameter->sensor_value_id)->findAll()[0];
+			// $sensor_value = @$sensor[@$sensor_value->sensor_reader_id * 1][@$sensor_value->pin * 1] * 1;
+			$sensor_value = 0;
 			$measurement_logs = [
 				"parameter_id" => $parameter->id,
 				"value" => ($data[$parameter->id] < 0) ? 0 : $data[$parameter->id],
