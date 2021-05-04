0b6e4d03a6353981bc0ef2c5e12f2ea462d058ae
warih suryono <warih_suryono@yahoo.com>
2021-05-04 15:10:39 +0700

    aaa

--- db_connect.py
+++ db_connect.py
@@ -1,14 +1,8 @@
-import sqlite3
+from mysql.connector.constants import ClientFlag
+import mysql.connector
 
 def connecting():
-    return sqlite3.connect('gui/app/Database/database.s3db')
-
-
-#from mysql.connector.constants import ClientFlag
-#import mysql.connector
-#
-#def connecting():
-#    try:
-#        return mysql.connector.connect(host="localhost",user="root",passwd="root",database="aqms_fs2")
-#    except Exception as e: 
-#        return false
+    try:
+        return mysql.connector.connect(host="localhost",user="root",passwd="root",database="aqms_fs2")
+    except Exception as e: 
+        return false
