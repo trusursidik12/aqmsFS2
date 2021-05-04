600031bfa824900421016f736b11e10a9fa3c598
warih suryono <warih_suryono@yahoo.com>
2021-05-04 16:38:40 +0700

    aaa

--- background_process.py
+++ background_process.py
@@ -7,7 +7,7 @@ while True:
     counter = counter + 1
     subprocess.Popen("php gui/spark command:formula_measurement_logs", shell=True)
     if(counter >= 60):
-        #subprocess.Popen("php gui/spark command:sentdata", shell=True)
+        subprocess.Popen("php gui/spark command:sentdata", shell=True)
         subprocess.Popen("php gui/spark command:measurement_averaging", shell=True)
         counter = 0
     time.sleep(1)
