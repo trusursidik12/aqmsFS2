15822edea8d030a59e9739b37c3f26d2e0782ae7
warih suryono <warih_suryono@yahoo.com>
2021-05-04 12:30:15 +0700

    aaa

--- background_process.py
+++ background_process.py
@@ -5,9 +5,9 @@ counter = 0
 
 while True:
     counter = counter + 1
-    subprocess.Popen("php gui/spark command:formula_measurement_logs", shell=False)
+    subprocess.Popen("cd gui && php spark command:formula_measurement_logs", shell=False)
     if(counter >= 60):
-        #subprocess.Popen("php gui/spark command:sentdata", shell=False)
-        subprocess.Popen("php gui/spark command:measurement_averaging", shell=False)
+        #subprocess.Popen("cd gui && php spark command:sentdata", shell=False)
+        subprocess.Popen("cd gui && php spark command:measurement_averaging", shell=False)
         counter = 0
     time.sleep(1)
