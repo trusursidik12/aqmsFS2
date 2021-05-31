c68157d8d31bb882484f1f778492de2225d51ec2
warih suryono <warih_suryono@yahoo.com>
2021-05-31 10:29:05 +0700

    aaa

--- gui/app/Database/Seeds/SensorReaders.php
+++ gui/app/Database/Seeds/SensorReaders.php
@@ -12,11 +12,12 @@ class SensorReaders extends Seeder
 		$data = [
 			['driver' => 'labjack_reader.py', 'sensor_code' => 'ANY', 'baud_rate' => '', 'pins' => '0,1,2,3'],
 			['driver' => 'hc.py', 'sensor_code' => '/dev/ttyUSB0', 'baud_rate' => '9600', 'pins' => ''],
-			['driver' => 'metone_82840.py', 'sensor_code' => '/dev/ttyUSB2', 'baud_rate' => '9600', 'pins' => ''],
-			['driver' => 'metone_82840.py', 'sensor_code' => '/dev/ttyUSB3', 'baud_rate' => '9600', 'pins' => ''],
+			['driver' => 'metone_integrated.py', 'sensor_code' => '/dev/ttyUSB0', 'baud_rate' => '9600', 'pins' => ''],
+			['driver' => 'metone_82840.py', 'sensor_code' => '', 'baud_rate' => '9600', 'pins' => ''],
+			['driver' => 'metone_82840.py', 'sensor_code' => '', 'baud_rate' => '9600', 'pins' => ''],
 			['driver' => 'vantagepro2.py', 'sensor_code' => '/dev/ttyUSB5', 'baud_rate' => '19200', 'pins' => ''],
 			['driver' => 'pwmpump_controller.py', 'sensor_code' => '/dev/ttyUSB6', 'baud_rate' => '9600', 'pins' => ''],
-			['driver' => 'pressure_sensor.py', 'sensor_code' => '/dev/ttyUSB1', 'baud_rate' => '9600', 'pins' => ''],
+			['driver' => 'pressure_sensor.py', 'sensor_code' => '', 'baud_rate' => '9600', 'pins' => ''],
 		];
 		$this->db->table('sensor_readers')->insertBatch($data);
 	}
