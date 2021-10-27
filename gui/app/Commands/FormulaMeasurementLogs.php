ec87d9db8d6b804405f1e5a36c1fc4a836106947
warihsuryono <warih_suryono@yahoo.com>
2021-10-27 17:20:24 +0700

    membrafor digital

--- gui/app/Commands/FormulaMeasurementLogs.php
+++ gui/app/Commands/FormulaMeasurementLogs.php
@@ -79,6 +79,14 @@ class FormulaMeasurementLogs extends BaseCommand
 	 * @param array $params
 	 */
 
+	public function hexToFloat($strHex)
+	{
+		$v = hexdec($strHex);
+		$x = ($v & ((1 << 23) - 1)) + (1 << 23) * ($v >> 31 | 1);
+		$exp = ($v >> 23 & 0xFF) - 127;
+		return $x * pow(2, $exp - 23);
+	}
+
 	public function run(array $params)
 	{
 		while (true) {
