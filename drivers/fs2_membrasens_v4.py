34d48f67081b2365cfec7650451d0743fdbfd0fe
warih suryono <warih_suryono@yahoo.com>
2021-10-28 15:40:59 +0700

    aaa

--- drivers/fs2_membrasens_v4.py
+++ drivers/fs2_membrasens_v4.py
@@ -100,7 +100,7 @@ try:
             # print(val)
             MEMBRAPOR = "FS2_MEMBRASENS;" + dectofloat(val[1],val[0]) + ";" + dectofloat(val[3],val[2]) + ";" + dectofloat(val[5],val[4]) + ";" + dectofloat(val[7],val[6]) + ";" + dectofloat(val[9],val[8]) + ";" + dectofloat(val[11],val[10]) + ";" + dectofloat(val[13],val[12]) + ";" + dectofloat(val[15],val[14]) + ";" + dectofloat(val[17],val[16]) + ";" + dectofloat(val[19],val[18]) + ";" + dectofloat(val[21],val[20]) + ";" + dectofloat(val[23],val[22]) + ";END;"            
             update_sensor_value(str(sys.argv[1]),str(MEMBRAPOR))
-            print(MEMBRAPOR)
+            # print(MEMBRAPOR)
         except Exception as e2:
             print(e2)
             print("Reconnect MEMBRAPOR");
