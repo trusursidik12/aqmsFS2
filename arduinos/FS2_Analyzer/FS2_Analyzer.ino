b147108f564a71b4b6bcc007c15e9f66ed56c379
warihsuryono <warih_suryono@yahoo.com>
2021-11-01 17:27:10 +0700

    aaa

--- arduinos/FS2_Analyzer/FS2_Analyzer.ino
+++ arduinos/FS2_Analyzer/FS2_Analyzer.ino
@@ -1,3 +1,7 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+
 String string_pm01 = "";
 String string_pm02 = "";
 
@@ -8,11 +12,18 @@ float temp1 = 0.0;
 float temp2 = 0.0;
 float hum1 = 0.0;
 float hum2 = 0.0;
+bool isSHT_1 = false;
+bool isSHT_2 = false;
+
+Adafruit_SHT31 sht31_1 = Adafruit_SHT31();
+Adafruit_SHT31 sht31_2 = Adafruit_SHT31();
+
 
 void setup() {
     Serial.begin(9600);
     Serial1.begin(9600);
     Serial2.begin(9600);
+    Serial.println("FS2_ANALYZER_START");
     pinMode(pinFanPwm, OUTPUT);
     delay(1000);
     digitalWrite(pinFanPwm, HIGH);
@@ -20,10 +31,53 @@ void setup() {
     digitalWrite(pinFanPwm, LOW);
     delay(1000);
     digitalWrite(pinFanPwm, HIGH);
+    Serial.println("Connecting SHT31 I...");
+    if (! sht31_1.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+      Serial.println("Couldn't find SHT31 I");
+    }else{
+      isSHT_1 = true;
+    }
+    delay(10);
+    Serial.println("Connecting SHT31 II...");
+    if (! sht31_2.begin(0x45)) {   // Set to 0x45 for alternate i2c addr
+      Serial.println("Couldn't find SHT31 II");
+    }else{
+      isSHT_2 = true;
+    }
+    delay(1000);
+    Serial.println("FS2_ANALYZER_BEGIN");
 }
 
 String getValue(String data)
 {
+    if(!isSHT_1){
+      if (sht31_1.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+        isSHT_1 = true;
+      }
+    }
+    
+    if(isSHT_1){
+      temp1 = sht31_1.readTemperature();
+      hum1 = sht31_1.readHumidity();
+    } else {
+      temp1 = 0.0;
+      hum1 = 0.0;
+    }
+    
+    if(!isSHT_2){
+      if (sht31_2.begin(0x45)) {   // Set to 0x45 for alternate i2c addr
+        isSHT_2 = true;
+      }
+    }
+    
+    if(isSHT_2){
+      temp2 = sht31_2.readTemperature();
+      hum2 = sht31_2.readHumidity();
+    } else {
+      temp2 = 0.0;
+      hum2 = 0.0;
+    }
+
     String retval = "";
     String temp = "";
     if(data.length() > 0){
