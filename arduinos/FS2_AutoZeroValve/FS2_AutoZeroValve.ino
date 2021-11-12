a438511e0747087b24160f70714f210f0a7fc90c
warihsuryono <warih_suryono@yahoo.com>
2021-11-12 20:22:02 +0700

    aaa

---
+++ arduinos/FS2_AutoZeroValve/FS2_AutoZeroValve.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+int valve1 = 12;
+int valve2 = 11;
+int activeValve = 1;
+char inChar;
+void setup() {
+    Serial.begin(9600);
+    pinMode(valve1, OUTPUT);
+    pinMode(valve2, OUTPUT);
+    Serial.println("FS2_AUTO_ZERO_VALVE_START");
+    digitalWrite(valve1,HIGH);
+    digitalWrite(valve2,LOW);
+    delay(1000);
+    digitalWrite(valve1,LOW);
+    digitalWrite(valve2,HIGH);
+    delay(1000);
+    digitalWrite(valve1,HIGH);
+    digitalWrite(valve2,LOW);
+    activeValve = 1;
+    delay(1000);
+    Serial.println("FS2_AUTO_ZERO_VALVE_BEGIN");
+    
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        inChar = Serial.read();
+        if(inChar == 'i'){
+            activeValve = 1;
+            digitalWrite(valve1,HIGH);
+            digitalWrite(valve2,LOW);
+        }
+        if(inChar == 'j'){
+            activeValve = 2;
+            digitalWrite(valve1,LOW);
+            digitalWrite(valve2,HIGH);
+        }
+    }
+    Serial.print("FS2_AUTO_ZERO_VALVE;");
+    Serial.print(activeValve);
+    Serial.print(";");
+    delay(1000);
+}
