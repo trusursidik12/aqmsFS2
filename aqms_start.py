7377573f4f79b5d48e7547349bcb000ca034eb9a
warihsuryono <warih_suryono@yahoo.com>
2021-11-22 13:25:30 +0700

    aaa

--- aqms_start.py
+++ aqms_start.py
@@ -89,10 +89,11 @@ def check_as_sds019(serialport):
         client.connect()
         result = client.read_holding_registers(address=0x00B4, count=3, unit=1)
         client.close()
-        
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_sds019.py' AND sensor_code='' LIMIT 1")
-        mydb.commit()
-        print(" ==> FS2_SDS019")
+        if(count(result.registers == 3):
+            mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_sds019.py' AND sensor_code='' LIMIT 1")
+            mydb.commit()
+            print(" ==> FS2_SDS019")
+            
         return None
     except Exception as e: 
         None
