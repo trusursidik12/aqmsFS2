4fbce574b5b958962bd82db1afca58fd7c8b0ef9
ir001 <irwanantonio2708@gmail.com>
2021-12-03 10:43:28 +0700

    rev zero calibrate & param span calibrate

--- gui/app/Views/calibrations/v_index.php
+++ gui/app/Views/calibrations/v_index.php
@@ -119,7 +119,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Calibration Logs</h4>
+                    <h4 class="card-title">Zero Calibration Logs</h4>
                 </div>
                 <div class="card-body">
                 <form class="form-inline">
--- gui/app/Views/parameter/v_index.php
+++ gui/app/Views/parameter/v_index.php
@@ -21,7 +21,7 @@
                         </div>
                         <p class="my-0"><?= $gas->formula ?></p>
                         <div class="d-flex justify-content-end">
-                            <button class="btn btn-sm btn-warning" data-id="<?= $gas->id ?>">
+                            <button class="btn btn-sm btn-warning btn-span-calibration" data-id="<?= $gas->id ?>">
                                 Span Calibration
                             </button>
                             &nbsp;&nbsp;&nbsp;
@@ -319,6 +319,81 @@
 
     </div>
 </div>
+
+<!-- Span Calibraton -->
+
+<div class="modal fade" id="spanModal" tabindex="-1" role="dialog" aria-labelledby="spanModalTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <form action="" method="post">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="spanModalTitle"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p class="alert alert-warning text-danger">
+                        <i class="fas fa-info-circle fa-xs mr-1"></i> Pastikan aliran gas kalibrasi span NO2 sudah terpasang dengan benar pada saluran sampling dengan laju alir 0.9 lpm
+                    </p>
+                    <p class="alert alert-warning text-danger">
+                        <i class="fas fa-info-circle fa-xs mr-1"></i> Kesalahan penggunaan gas kalibrasi dapat mempengaruhi daya akurasi pengukuran pada sensor Gas
+                    </p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Cylinder Gas No</label>
+                                <input type="hidden" name="id">
+                                <input type="text" name="cylinder_gas_no" value="<?= old('cylinder_gas_no') ?>" placeholder="Cylinder Gas No" class="form-control">
+                                <div class="invalid-feedback">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Span Concetration</label>
+                                <input type="text" name="span_concetration" value="<?= old('span_concetration') ?>" placeholder="Span Concetration" class="form-control">
+                                <div class="invalid-feedback">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input type="text" name="username" value="<?= old('username') ?>" placeholder="Username" class="form-control">
+                                <div class="invalid-feedback">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" name="password" value="<?= old('password') ?>" placeholder="Password" class="form-control">
+                                <div class="invalid-feedback">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <div class="d-flex justify-content-end">
+                        <button name="Save" type="submit" class="btn btn-sm btn-primary mr-1">Start</button>
+                        <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</div>
 <?= $this->endSection() ?>
 <?= $this->section('css') ?>
 <!-- Custom CSS Here -->
@@ -381,6 +456,31 @@
 
             }
         });
+        // Span Calibration
+        $('.btn-span-calibration').click(function(){
+            let param_id = $(this).attr('data-id');
+            var btnEdit = $(this);
+            btnEdit.html(`<i class="fas fa-spinner fa-spin"></i>`);
+            try{
+                $.ajax({
+                    url: '<?= base_url('parameter/detail') ?>',
+                    data: {
+                        id: param_id
+                    },
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.success) {
+                            let parameter = data.data;
+                            $('#spanModalTitle').html(`${parameter?.caption_id} Span Calibration`)
+                            $('#spanModal').modal('show');
+                            btnEdit.html(`Span Calibration`);
+                        }
+                    }
+                })
+            }catch(err){
+                console.log(err);
+            }
+        })
 
         $('#sensor_value_id').change(function() {
             clearInterval(intervalChange);
