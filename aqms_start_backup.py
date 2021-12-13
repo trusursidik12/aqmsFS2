f44ac5cadb97b859645fe945f7ff1a80db514c50
dulurprojects2019 <trusursidik12@gmail.com>
2021-12-13 10:36:53 +0700

    initial ws

--- aqms_start_backup.py
+++ aqms_start_backup.py
@@ -33,8 +33,7 @@ def serial_ports():
         except (OSError, serial.SerialException):
             pass
     return result
-
-
+    
 def check_as_arduino(port):
     COM = serial.Serial()
     COM.port = port
@@ -43,55 +42,47 @@ def check_as_arduino(port):
     COM.open()
     retval = str(COM.readline())
     if(retval.count("FS2_ANALYZER") > 0):
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port +
-                         "' WHERE driver LIKE 'fs2_analyzer_module.py' AND sensor_code='' LIMIT 1")
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_analyzer_module.py' AND sensor_code='' LIMIT 1")
         mydb.commit()
         print(" ==> FS2_ANALYZER")
-
+        
     if(retval.count("FS2_PUMP") > 0):
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port +
-                         "' WHERE driver LIKE 'fs2_pump_module.py' AND sensor_code='' LIMIT 1")
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_pump_module.py' AND sensor_code='' LIMIT 1")
         mydb.commit()
         print(" ==> FS2_PUMP")
-
+        
     if(retval.count("FS2_PSU") > 0):
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port +
-                         "' WHERE driver LIKE 'fs2_psu_module.py' AND sensor_code='' LIMIT 1")
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_psu_module.py' AND sensor_code='' LIMIT 1")
         mydb.commit()
         print(" ==> FS2_PSU")
-
+        
     if(retval.count("FS2_AUTO_ZERO_VALVE") > 0):
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port +
-                         "' WHERE driver LIKE 'fs2_autozerovalve.py' AND sensor_code='' LIMIT 1")
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_autozerovalve.py' AND sensor_code='' LIMIT 1")
         mydb.commit()
         print(" ==> FS2_AUTO_ZERO_VALVE")
-
-
+        
 def check_as_membrasens(port):
     try:
-        rs485 = minimalmodbus.Instrument(port, 1)
-        rs485.serial.baudrate = 19200
-        rs485.serial.parity = serial.PARITY_EVEN
-        rs485.serial.bytesize = 8
-        rs485.serial.stopbits = 1
-        rs485.mode = minimalmodbus.MODE_RTU
-        rs485.serial.timeout = 0.2
-
-        regConcentration = rs485.read_registers(1000, 8, 3)
-        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port +
-                         "' WHERE driver LIKE 'fs2_membrasens_v4.py' AND sensor_code='' LIMIT 1")
+        rs485=minimalmodbus.Instrument(port,1)
+        rs485.serial.baudrate=19200
+        rs485.serial.parity=serial.PARITY_EVEN
+        rs485.serial.bytesize=8
+        rs485.serial.stopbits=1
+        rs485.mode=minimalmodbus.MODE_RTU
+        rs485.serial.timeout=0.2
+        
+        regConcentration = rs485.read_registers(1000,8,3)
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'fs2_membrasens_v4.py' AND sensor_code='' LIMIT 1")
         mydb.commit()
         print(" ==> FS2_MEMBRASENS_V4")
         return None
-    except Exception as e:
+    except Exception as e: 
         None
-
-
+        
 def check_as_sds019(serialport):
     try:
         ser = serial.rs485.RS485(port=serialport, baudrate=9600, timeout=3)
-        ser.rs485_mode = serial.rs485.RS485Settings(
-            rts_level_for_tx=False, rts_level_for_rx=True, delay_before_tx=0.0, delay_before_rx=-0.0)
+        ser.rs485_mode = serial.rs485.RS485Settings(rts_level_for_tx=False, rts_level_for_rx=True, delay_before_tx=0.0, delay_before_rx=-0.0)
         ser.timeout = 0.5
         client = ModbusSerialClient(method='rtu')
         client.socket = ser
@@ -99,32 +90,27 @@ def check_as_sds019(serialport):
         client.connect()
         result = client.read_holding_registers(address=0x00B4, count=3, unit=1)
         if(len(result.registers) == 3):
-            mycursor.execute("UPDATE sensor_readers SET sensor_code='" + serialport +
-                             "' WHERE driver LIKE 'fs2_sds019.py' AND sensor_code='' LIMIT 1")
+            mycursor.execute("UPDATE sensor_readers SET sensor_code='" + serialport + "' WHERE driver LIKE 'fs2_sds019.py' AND sensor_code='' LIMIT 1")
             mydb.commit()
             print(" ==> FS2_SDS019")
-
+            
         return None
-    except Exception as e:
+    except Exception as e: 
         None
-
-
+        
 def check_as_ventagepro2(port):
     try:
         COM_WS = VantagePro2.from_url("serial:%s:%s:8N1" % (port, 19200))
         ws_data = COM_WS.get_current_data()
-        WS = ws_data.to_csv(';', False)
-        mycursor.execute(
-            "UPDATE sensor_readers SET sensor_code='/dev/ttyWS' WHERE driver LIKE 'vantagepro2.py' AND sensor_code='' LIMIT 1")
-        mydb.commit()
+        WS = ws_data.to_csv(';',False)
+        mycursor.execute("UPDATE sensor_readers SET sensor_code='" + port + "' WHERE driver LIKE 'vantagepro2.py' AND sensor_code='' LIMIT 1")
+        mydb.commit() 
         print(" ==> VANTAGEPRO2")
-    except Exception as e:
+    except Exception as e: 
         None
 
-
-# =============================AUTO DETECT SERIAL PORTS=================================
-mycursor.execute(
-    "UPDATE sensor_readers SET sensor_code='' WHERE driver != 'vantagepro2.py'")
+##=============================AUTO DETECT SERIAL PORTS=================================
+mycursor.execute("UPDATE sensor_readers SET sensor_code=''")
 mydb.commit()
 mycursor.execute("TRUNCATE TABLE serial_ports")
 mydb.commit()
@@ -135,8 +121,7 @@ for port in serial_ports():
     port_desc = ""
 
     if sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        p = subprocess.Popen('dmesg | grep ' + str(port).replace('/dev/',
-                             '') + ' | tail -1', stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('dmesg | grep ' + str(port).replace('/dev/','') + ' | tail -1', stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         p_status = p.wait()
         port_desc = output.decode("utf-8")
@@ -148,72 +133,65 @@ for port in serial_ports():
 
     print(port_desc)
     try:
-        mycursor.execute(
-            "INSERT INTO serial_ports (port,description) VALUES ('" + port + "','" + port_desc + "')")
+        mycursor.execute("INSERT INTO serial_ports (port,description) VALUES ('" + port +"','" + port_desc +"')")
         mydb.commit()
-    except Exception as e:
+    except Exception as e: 
         None
-
+    
 mycursor.execute("SELECT port,description FROM serial_ports ORDER BY port")
 serial_ports = mycursor.fetchall()
 for serial_port in serial_ports:
     print(serial_port[0])
     if(str(serial_port[0]).count("ttyUSB") > 0 or str(serial_port[0]).count("ttyACM") > 0 or str(serial_port[0]).count("COM") > 0):
         check_as_membrasens(serial_port[0])
-
-        mycursor.execute(
-            "SELECT id FROM sensor_readers WHERE sensor_code = '" + serial_port[0] + "'")
+        
+        mycursor.execute("SELECT id FROM sensor_readers WHERE sensor_code = '"+ serial_port[0] +"'")
         try:
             sensor_reader_id = mycursor.fetchone()[0]
         except Exception as e:
             sensor_reader_id = ""
         if(str(sensor_reader_id) == ""):
             check_as_sds019(serial_port[0])
-
-        mycursor.execute(
-            "SELECT id FROM sensor_readers WHERE sensor_code = '" + serial_port[0] + "'")
+        
+        mycursor.execute("SELECT id FROM sensor_readers WHERE sensor_code = '"+ serial_port[0] +"'")
         try:
             sensor_reader_id = mycursor.fetchone()[0]
         except Exception as e:
             sensor_reader_id = ""
         if(str(sensor_reader_id) == ""):
             check_as_ventagepro2(serial_port[0])
-
-        mycursor.execute(
-            "SELECT id FROM sensor_readers WHERE sensor_code = '" + serial_port[0] + "'")
+        
+        mycursor.execute("SELECT id FROM sensor_readers WHERE sensor_code = '"+ serial_port[0] +"'")
         try:
             sensor_reader_id = mycursor.fetchone()[0]
         except Exception as e:
             sensor_reader_id = ""
-        if(str(sensor_reader_id) == ""):
+        if(str(sensor_reader_id) == ""):    
             time.sleep(5)
             check_as_arduino(serial_port[0])
-
-# =============================END AUTO DETECT SERIAL PORTS=================================
-mycursor.execute("TRUNCATE sensor_values")
+            
+##=============================END AUTO DETECT SERIAL PORTS=================================
+mycursor.execute("TRUNCATE sensor_values");
 mydb.commit()
-mycursor.execute("TRUNCATE measurement_logs")
+mycursor.execute("TRUNCATE measurement_logs");
 mydb.commit()
 
 
 subprocess.Popen("php gui/spark serve", shell=True)
 time.sleep(1)
 
-mycursor.execute(
-    "UPDATE configurations SET content=NOW() WHERE name LIKE 'pump_last'")
+mycursor.execute("UPDATE configurations SET content=NOW() WHERE name LIKE 'pump_last'")
 mydb.commit()
 
-mycursor.execute(
-    "SELECT id,driver FROM sensor_readers WHERE sensor_code <> ''")
+mycursor.execute("SELECT id,driver FROM sensor_readers WHERE sensor_code <> ''")
 sensor_readers = mycursor.fetchall()
 for sensor_reader in sensor_readers:
     time.sleep(3)
-    command = "python drivers/" + \
-        sensor_reader[1] + " " + str(sensor_reader[0])
+    command = "python drivers/" + sensor_reader[1] + " " + str(sensor_reader[0])
     if sys.platform.startswith('win') == False:
-        command = command.replace("python", "python3")
+        command = command.replace("python","python3")
     subprocess.Popen(command, shell=True)
-
+    
 
 subprocess.Popen("php gui/spark command:formula_measurement_logs", shell=True)
 print("php gui/spark command:formula_measurement_logs")
