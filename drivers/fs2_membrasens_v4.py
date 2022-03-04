c6f9c1feab2a68e5245b062f1fc1111389971847
warihsuryono <warih_suryono@yahoo.com>
2022-03-04 21:11:50 +0700

    aaa

--- drivers/fs2_membrasens_v4.py
+++ drivers/fs2_membrasens_v4.py
@@ -90,6 +90,8 @@ def zeroing():
         time.sleep(1)
         rs485.write_registers(1220,[0,0,0,0])
         time.sleep(3)
+        rs485.write_registers(1210,[0,0,0,0])
+        time.sleep(3)
         
         
         mycursor.execute("SELECT content FROM configurations WHERE name LIKE 'calibrator_name'")
