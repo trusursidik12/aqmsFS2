b1da1f34c8009820e850a19a4200d850f98c307d
warihsuryono <warih_suryono@yahoo.com>
2022-03-07 11:30:56 +0700

    aaa

--- drivers/fs2_membrasens_v4.py
+++ drivers/fs2_membrasens_v4.py
@@ -153,14 +153,14 @@ def check_is_span():
             rs485.write_registers(1200,[0])
             time.sleep(3)
             # rs485.write_registers(spanAddress,[span])
-            rs485.write_register(spanAddress,span)
+            rs485.write_register(spanAddress,span,unit=1)
             time.sleep(3)
             # rs485.write_registers(1210,[0,0,0,0])
             # time.sleep(3)
             print("Span Ended")
         
     except Exception as e:
-        # print(e)
+        print(e)
         return None
         
 
