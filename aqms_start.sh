47f41b222b742f7df4b7394c8c4b227436524979
warihsuryono <warih_suryono@yahoo.com>
2022-04-05 14:16:24 +0700

    ganti password mysql

--- aqms_start.sh
+++ aqms_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Reading devices... Please wait!!"
 sleep 1s
+ll /dev/ttyUSB*
+ll /dev/ttyM*
+sleep 1s
+echo admin | sudo -S chmod 666 /dev/tty*
+sleep 1s
 cd ~/aqmsFS2/ && python3 aqms_start.py
 $SHELL
--- drivers/db_connect.py
+++ drivers/db_connect.py
@@ -3,6 +3,6 @@ import mysql.connector
 
 def connecting():
     try:
-        return mysql.connector.connect(host="localhost",user="root",passwd="root",database="aqms_fs2")
+        return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_fs2")
     except Exception as e: 
         return false
