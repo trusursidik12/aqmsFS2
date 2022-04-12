221b946ccc2d07b1b1edac677395da08137485cc
dzikri <dzikri.subagja02@gmail.com>
2022-04-12 09:44:30 +0700

    no zero concentration

--- drivers/fs2_membrasens_v4.py
+++ drivers/fs2_membrasens_v4.py
@@ -205,8 +205,14 @@ try:
             # print(val)
             if(dectofloat(val[1],val[0]) != "0"):
                 concentration0 = dectofloat(val[1],val[0])
+            if(dectofloat(val[3],val[2]) != "0"):
+                concentration1 = dectofloat(val[3],val[2])
+            if(dectofloat(val[5],val[4]) != "0"):
+                concentration2 = dectofloat(val[5],val[4])
+            if(dectofloat(val[7],val[6]) != "0"):
+                concentration3 = dectofloat(val[7],val[6])
                 
-            MEMBRAPOR = "FS2_MEMBRASENS;" + concentration0 + ";" + dectofloat(val[3],val[2]) + ";" + dectofloat(val[5],val[4]) + ";" + dectofloat(val[7],val[6]) + ";" + dectofloat(val[9],val[8]) + ";" + dectofloat(val[11],val[10]) + ";" + dectofloat(val[13],val[12]) + ";" + dectofloat(val[15],val[14]) + ";" + dectofloat(val[17],val[16]) + ";" + dectofloat(val[19],val[18]) + ";" + dectofloat(val[21],val[20]) + ";" + dectofloat(val[23],val[22]) + ";END;"            
+            MEMBRAPOR = "FS2_MEMBRASENS;" + concentration0 + ";" + concentration1 + ";" + concentration2 + ";" + concentration3 + ";" + dectofloat(val[9],val[8]) + ";" + dectofloat(val[11],val[10]) + ";" + dectofloat(val[13],val[12]) + ";" + dectofloat(val[15],val[14]) + ";" + dectofloat(val[17],val[16]) + ";" + dectofloat(val[19],val[18]) + ";" + dectofloat(val[21],val[20]) + ";" + dectofloat(val[23],val[22]) + ";END;"            
             update_sensor_value(str(sys.argv[1]),str(MEMBRAPOR))
             # print(MEMBRAPOR)
         except Exception as e2:
