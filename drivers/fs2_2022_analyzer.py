1e66a7ce5006b491da25b0c8862e433ebf7f922e
warihsuryono <warih_suryono@yahoo.com>
2022-05-19 14:51:37 +0700

    aa

--- drivers/fs2_2022_analyzer.py
+++ drivers/fs2_2022_analyzer.py
@@ -46,13 +46,13 @@ def connect_analyzer():
             print("[V] ANALYZER Module " + sensor_reader[0] + " CONNECTED")
 
             COM_ANALYZER.write(str("$FAN,255#").encode())
-            print(COM_ANALYZER.read_until(str("#").encode()))
+            COM_ANALYZER.read_until(str("#").encode())
 
             COM_ANALYZER.write(str("$SHT31,BEGIN#").encode())
-            print(COM_ANALYZER.read_until(str("#").encode()))
+            COM_ANALYZER.read_until(str("#").encode())
 
             COM_ANALYZER.write(str("$SHT31,SET,AUTO#").encode())
-            print(COM_ANALYZER.read_until(str("#").encode()))
+            COM_ANALYZER.read_until(str("#").encode())
             return COM_ANALYZER
         else:
             is_ANALYZER_connect = False
