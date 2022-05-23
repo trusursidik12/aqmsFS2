6ed91e84b04fe6fc4e898e31aaf649d12dd23756
warihsuryono <warih_suryono@yahoo.com>
2022-05-23 15:04:06 +0700

    aaa

--- killer.sh
+++ killer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Killing all"
 sleep 1s
-echo admin | sudo kill -S $(ps aux | grep '[p]hp' | awk '{print $2}')
+echo admin | sudo -S kill -9 $(ps aux | grep '[p]hp' | awk '{print $2}')
 sleep 1s
-echo admin | sudo kill -S $(ps aux | grep 'terminal' | awk '{print $2}')
+echo admin | sudo -S kill -9 $(ps aux | grep 'terminal' | awk '{print $2}')
 sleep 1s
-echo admin | sudo kill -S $(ps aux | grep 'firefox' | awk '{print $2}')
+echo admin | sudo -S kill -9 $(ps aux | grep 'firefox' | awk '{print $2}')
