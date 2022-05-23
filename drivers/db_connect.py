c25826b739bcbd3be8fc81d3c762ed5179c2ade1
warihsuryono <warih_suryono@yahoo.com>
2022-05-23 14:04:14 +0700

    aaa

--- drivers/db_connect.py
+++ drivers/db_connect.py
@@ -6,6 +6,6 @@ def connecting():
     try:
         return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_fs2")
     except Exception as e: 
-        subprocess.Popen("echo admin | kill -S $(ps aux | grep '[p]hp' | awk '{print $2}')", shell=True)
-        subprocess.Popen("echo admin | kill -S $(ps aux | grep '[p]ython' | awk '{print $2}')", shell=True)
+        subprocess.Popen("echo admin | sudo kill -S $(ps aux | grep '[p]hp' | awk '{print $2}')", shell=True)
+        subprocess.Popen("echo admin | sudo kill -S $(ps aux | grep '[p]ython' | awk '{print $2}')", shell=True)
         return false
