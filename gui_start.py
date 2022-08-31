787ee66ebb7916eebd1d6cba6df5421bb4b74ad8
warihsuryono <warih_suryono@yahoo.com>
2022-08-31 09:44:44 +0700

    aaa'

--- gui_start.py
+++ gui_start.py
@@ -1,3 +1,4 @@
 import subprocess
 subprocess.Popen("chromium --fullscreen --kiosk http://localhost:8080", shell=True)
-#subprocess.Popen("chromium-browser --fullscreen --kiosk http://localhost:8080", shell=True)
+# subprocess.Popen("firefox --kiosk=http://localhost:8080", shell=True)
+# subprocess.Popen("chromium-browser --fullscreen --kiosk http://localhost:8080", shell=True)
