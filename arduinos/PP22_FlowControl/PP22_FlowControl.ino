121552e49a602908dff20e6e6a1c088a8b13d39c
dzikri <dzikri.subagja02@gmail.com>
2022-10-24 14:25:30 +0700

    Create PP22_FlowControl.ino
    Arduino Flow Control

---
+++ arduinos/PP22_FlowControl/PP22_FlowControl.ino
@@ -0,0 +1,72 @@
+int ntcpin = A0;
+int flowpin = A1;
+int pwmpin = 10;
+int heaterpin = 12;
+int adjpin = 0;
+int pumpspeed = 60;
+int sccm = 0;
+int adj = 0;
+int ntc = 0;
+int ntcmax = 800;
+int ntcmin = 700;
+int heaterstate = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pwmpin, OUTPUT);
+  pinMode(heaterpin, OUTPUT);
+  digitalWrite(heaterpin, HIGH);
+  delay(1000);
+  digitalWrite(heaterpin, LOW);
+}
+
+void loop() {
+    //Pump
+    sccm = map(analogRead(flowpin),0,1023,0,3000);
+    adj = map(analogRead(adjpin),0,1023,-512,512);
+    sccm = sccm + adj;
+    Serial.println("Adj \t\t= " + String(adj)); 
+    Serial.println("Pumpspeed \t= " + String(pumpspeed));
+    Serial.println("Sccm \t\t= " + String(sccm));
+    if (sccm < 1990){
+      pumpspeed++;
+    }
+    if (sccm > 2010){
+      pumpspeed --;
+    }
+    analogWrite(pwmpin, map(pumpspeed,0,100,0,255));
+
+
+
+
+    //heater
+    ntc = analogRead(ntcpin);
+    if (ntc > ntcmax){
+      heaterstate = LOW;
+    }
+    if (ntc < ntcmin){
+      heaterstate = HIGH;
+    }
+    Serial.println("ntc \t\t= " + String(ntc));
+    Serial.println("Heaterstate \t= " + String(heaterstate));
+    digitalWrite(heaterpin, heaterstate);
+    Serial.println("");
+    
+
+
+
+
+  /*
+  //if (Serial.available() > 0) {
+  //pumpspeed = Serial.readString().toInt();
+  pumpspeed = map(analogRead(ntcpin),0,1024,0,512);
+  Serial.print(map(pumpspeed,0,511,0,100));
+  Serial.print(":");
+  Serial.print(pumpspeed);
+  Serial.print(":");
+  Serial.println(analogRead(ntcpin));
+  analogWrite(pwmpin, pumpspeed);
+  //}
+  */
+  delay(1000);
+}
