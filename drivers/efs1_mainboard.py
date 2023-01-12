a70acb1226f91f49a8870adde636dac0dfac605e
warihsuryono <warih_suryono@yahoo.com>
2023-01-12 09:45:52 +0700

    aaa

---
+++ drivers/efs1_mainboard.py
@@ -0,0 +1,183 @@
+from __future__ import print_function
+import sys
+import serial
+import time
+import db_connect
+
+is_SENSOR_connect = False
+is_connect = False
+current_state = 0
+current_speed = 0
+pump_state = 0
+sensor_mode = [""] * 10
+end_string_sensor = [""] * 10
+
+sensor_mode[0] = "data.membrasens.ppm"
+sensor_mode[1] = "data.membrasens.temp"
+sensor_mode[2] = "data.pm.1"
+sensor_mode[3] = "data.pm.2"
+sensor_mode[4] = "data.ina219"
+sensor_mode[5] = "data.dht"
+sensor_mode[6] = "data.bme"
+sensor_mode[7] = "data.sentec"
+sensor_mode[8] = "data.pressure"
+sensor_mode[9] = "data.pump"
+
+end_string_sensor[0] = "END_MEMBRASENS_PPM"
+end_string_sensor[1] = "END_MEMBRASENS_TEMP"
+end_string_sensor[2] = "END_PM1"
+end_string_sensor[3] = "END_PM2"
+end_string_sensor[4] = "END_INA219"
+end_string_sensor[5] = "END_DHT"
+end_string_sensor[6] = "END_BME"
+end_string_sensor[7] = "END_SENTEC"
+end_string_sensor[8] = "END_PRESSURE"
+end_string_sensor[9] = "END_PUMP"
+
+try:
+    mydb = db_connect.connecting()
+    mycursor = mydb.cursor()
+
+    mycursor.execute(
+        "SELECT sensor_code,baud_rate FROM sensor_readers WHERE id = '" + sys.argv[1] + "'")
+    sensor_reader = mycursor.fetchone()
+except Exception as e:
+    print("[X]  SENSOR Module ID: " + str(sys.argv[1]) + " " + e)
+
+
+def update_sensor_value(sensor_reader_id, pin, value):
+    try:
+        try:
+            mycursor.execute("SELECT id FROM sensor_values WHERE sensor_reader_id = '" +
+                             str(sensor_reader_id) + "' AND pin = '" +
+                             str(pin) + "'")
+            sensor_value_id = mycursor.fetchone()[0]
+            mycursor.execute("UPDATE sensor_values SET value = '" +
+                             str(value) + "' WHERE id = '" + str(sensor_value_id) + "'")
+            mydb.commit()
+        except Exception as e:
+            mycursor.execute("INSERT INTO sensor_values (sensor_reader_id,pin,value) VALUES ('" +
+                             str(sensor_reader_id) + "','" +
+                             str(pin) + "','" + str(value) + "')")
+            mydb.commit()
+    except Exception as e2:
+        print("Error update_sensor_value")
+        print(e2)
+        return None
+
+try:
+    mycursor.execute(
+        "SELECT sensor_code,baud_rate FROM sensor_readers WHERE id = '" + sys.argv[1] + "'")
+    sensor_reader = mycursor.fetchone()
+    ser = serial.Serial(sensor_reader[0], sensor_reader[1], timeout=5)
+except Exception as e:
+    ser = None
+
+def getSensorValue(mode):
+    retval = ""
+    try:
+        while retval.find(end_string_sensor[mode]) == -1:
+            ser.write(bytes(sensor_mode[mode] + "#",'utf-8'))
+            time.sleep(1)
+            retval = ser.readline().decode('utf-8').strip('/r/n')
+    except Exception as e:
+        None
+        
+    return retval
+    
+    
+def update_all_sensor():
+    for mode in range(10):
+        try:
+            value = getSensorValue(mode)
+            # print(value)
+            update_sensor_value(str(sys.argv[1]), mode, value)
+        except Exception as e:
+            None
+            
+        time.sleep(1)
+        
+def check_is_switch():
+    global pump_state
+    global is_connect
+    global current_state
+    try:
+        try:
+            mycursor.execute(
+                "SELECT content FROM configurations WHERE name LIKE 'pump_state'")
+            pump_state = mycursor.fetchone()[0]
+            # print(pump_state)
+            is_connect = True
+            if pump_state != current_state and is_connect:
+                current_state = pump_state
+                if (current_state == '0'):
+                    ser.write(b'switch,0,*')
+                elif (current_state == '1'):
+                    ser.write(b'switch,1,*')
+                data = ser.readline().decode('utf-8').strip('/r/n')
+                print(data)
+                return (data)
+            return int(pump_state[0])
+        except Exception as e4:
+            pump_state = ""
+            is_connect = False
+            print(e4)
+        print('Pump Connect:', is_connect)
+    except Exception as e:
+        print(e)
+
+
+def set_pwm():
+    global current_speed
+    mycursor.execute(
+        "SELECT content FROM configurations WHERE name LIKE 'pump_speed'")
+    pump_speed = mycursor.fetchone()[0]
+    is_connect = True
+    if pump_speed != current_speed and is_connect:
+        current_speed = pump_speed
+        data = 'setPWM,' + str(current_speed) + ',*'
+        kutip = b''
+        returnval = kutip.decode('utf-8') + data
+        ser.write(returnval.encode('ascii'))
+        time.sleep(1)
+        ser.write(returnval.encode('ascii'))
+    data = ser.readline().decode('utf-8').strip('/r/n')
+    print(data)
+
+
+try:
+    while True:
+        print(ser)
+        if ser is not None:
+            update_all_sensor()
+        else:
+            try:
+                mycursor.execute(
+                    "SELECT sensor_code,baud_rate FROM sensor_readers WHERE id = '" + sys.argv[1] + "'")
+                sensor_reader = mycursor.fetchone()
+                ser = serial.Serial(sensor_reader[0], sensor_reader[1], timeout=5)
+            except Exception as e:
+                ser = None
+            
+            
+        # set_pwm()
+        # check_is_switch()
+        # try:
+            # val = read_concentration()
+            # print(val)
+        # except Exception as e3:
+            # print(" : error val = connect_sensor()")
+            # print(e3)
+
+        # try:
+            # SENSOR = "MC," + str(val) + ",END,"
+            # MC,NO2,SO2,O3,CO2,VOC,PWM,Volt,Current,Power,WD(derajat)
+            # update_sensor_value(str(sys.argv[1]), str(SENSOR))
+        # except Exception as e3:
+            # print("SENSOR value error")
+        
+        
+        time.sleep(1)
+
+except Exception as e:
+    print(e)
