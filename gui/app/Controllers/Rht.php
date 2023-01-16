3150a7a4799785a040535712938d80e2251f68ed
Dzikrish <dzikri.subagja02@gmail.com>
2023-01-16 14:04:15 +0700

    Mencoba span untuk board 2 dengan motherboard

--- gui/app/Controllers/Rht.php
+++ gui/app/Controllers/Rht.php
@@ -136,8 +136,7 @@ class Rht extends BaseController
 			if ($is_motherboard == "1") {
 				$sensor_reader_id = @$this->sensor_values->where("value LIKE '%MEMBRASENS_PPM%'")->findAll()[$board]->sensor_reader_id;
 				if ($board == "1") {
-					$board == "0";
-					$port += 4;
+					$port += "4";
 				}
 			} else {
 				$sensor_reader_id = @$this->sensor_values->where("value LIKE '%FS2_MEMBRASENS%'")->findAll()[$board]->sensor_reader_id;
