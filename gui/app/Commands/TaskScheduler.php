c227b278665344559c583518d4fab03b06e9a944
warih suryono <warih_suryono@yahoo.com>
2023-11-07 13:03:48 +0700

    aaa

--- gui/app/Commands/TaskScheduler.php
+++ gui/app/Commands/TaskScheduler.php
@@ -3,8 +3,8 @@
 namespace App\Commands;
 
 use CodeIgniter\CLI\BaseCommand;
-use CodeIgniter\CLI\CLI;
 use App\Models\m_configuration;
+use App\Models\m_parameter;
 use App\Models\m_measurement_log;
 use Exception;
 
@@ -18,12 +18,14 @@ class TaskScheduler extends BaseCommand
 	protected $group = 'CodeIgniter';
 
 	protected $configurations;
+	protected $parameters;
 	protected $measurement_logs;
 
 
 	public function __construct()
 	{
 		$this->configurations =  new m_configuration();
+		$this->parameters =  new m_parameter();
 		$this->measurement_logs =  new m_measurement_log();
 	}
 	/**
@@ -68,6 +70,7 @@ class TaskScheduler extends BaseCommand
 	 */
 	public function run(array $params)
 	{
+		$curl = \Config\Services::curlrequest();
 		while (true) {
 			try {
 				$restart_schedule = @$this->configurations->where(["name" => "restart_schedule"])->find()[0]->content;
@@ -84,12 +87,11 @@ class TaskScheduler extends BaseCommand
 			try {
 				if (!isset($counter)) $counter = 59;
 				$counter++;
-				if ($counter >= 60) {
+				if ($counter >= 30) {
 					$counter = -1;
 					$id_stasiun = @$this->configurations->where(["name" => "id_stasiun"])->first()->content;
-					$parameter_ids = [1, 2, 3, 4, 5, 12, 14];
-					foreach ($parameter_ids as $parameter_id) {
-						// for ($parameter_id = 1; $parameter_id < 6; $parameter_id++) {
+					foreach ($this->parameters->where("is_view = 1 AND p_type IN ('gas','particulate')")->findAll() as $parameter) {
+						$parameter_id = $parameter->id;
 						if (!isset($is_anomaly[$parameter_id])) $is_anomaly[$parameter_id] = true;
 						foreach ($this->measurement_logs->where("parameter_id", $parameter_id)->orderBy("id DESC")->findAll(20) as $key => $measurement_log) {
 							if ($key == 0) { //cek timestamp pertama
@@ -112,19 +114,7 @@ class TaskScheduler extends BaseCommand
 						if ($is_anomaly_) $status_codes .= ";43" . $parameter_id;
 					}
 
-					$curl = curl_init();
-					curl_setopt_array($curl, array(
-						CURLOPT_URL => "https://dashboards.trusur.tech/api/aqms/push_status/" . $id_stasiun . $status_codes,
-						CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_MAXREDIRS => 10,
-						CURLOPT_TIMEOUT => 30,
-						CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-						CURLOPT_CUSTOMREQUEST => "GET",
-						CURLOPT_SSL_VERIFYPEER => 0, //skip SSL Verification | disable SSL verify peer
-					));
-					$response = curl_exec($curl);
-					$err = curl_error($curl);
-					curl_close($curl);
+					$response = $curl->request('GET', "https://dashboards.trusur.tech/api/aqms/push_status/" . $id_stasiun . $status_codes, ['version' => 1.1, 'verify' => false]);
 				}
 			} catch (Exception $e) {
 			}
